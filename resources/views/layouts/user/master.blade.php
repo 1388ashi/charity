782684d5261f89c0f7b0b655c88cbd72dd57c514
arshia88 <arshiabat5@gmail.com>
2025-09-20 12:05:30 +0330

    rename title

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>{{ config('app.name') }}</title>
+    <title>پنل مدیریت</title>
 
     <!--Favicon -->
     <link rel="icon" href="{{asset('assets/images/brand/favicon.ico')}}" type="image/x-icon"/>
