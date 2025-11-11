7e69c590be185f1d861806c998480567f13353cc
arshia88 <arshiabat5@gmail.com>
2025-11-11 19:22:24 +0330

    modify some bug

--- resources/views/layouts/companion/footer.blade.php
+++
@@ -1,2 +0,0 @@
- <hr/>
-<a target="_blank" href="https://trustseal.enamad.ir/?id=670150&Code=tz2EC3pRVD2SxwvIEK3K0tOZc3MhRid6"><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=670150&Code=tz2EC3pRVD2SxwvIEK3K0tOZc3MhRid6' alt='' style='cursor:pointer' code='tz2EC3pRVD2SxwvIEK3K0tOZc3MhRid6'></a>
